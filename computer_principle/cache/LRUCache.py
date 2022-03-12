5b7f9468116f7175902bb700d559e872de7c53d3
bamboosky2021 <bamboosky2021@163.com>
2022-03-12 14:55:37 +0800

    LRU logic

---
+++ computer_principle/cache/LRUCache.py
@@ -0,0 +1,60 @@
+#！ -*- encoding=utf-8 -*-
+import sys
+sys.path.append("C:/Users/ADMIN/Desktop/git_computerbasic/computer_basic/computer_principle/DoubleLinked")
+
+from DoubleLinkedList import DoubleLinkedList,Node
+
+class LRUCache(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.map = {}
+        self.list = DoubleLinkedList(self.capacity)
+
+    def get(self, key):
+        if key in self.map:
+            node = self.map[key]
+            self.list.remove(node)
+            self.list.append_front(node)
+            return node.value
+        else:
+            return -1
+
+    def put(self, key, value):
+        if key in self.map:
+            node = self.map.get(key)
+            self.list.remove(node)
+            node.value = value
+            self.list.append_front(node)
+        else:
+            node = Node(key, value)
+            # capacity reached
+            if self.list.size >= self.list.capacity:
+                old_node = self.list.remove()
+                self.map.pop(old_node.key)
+                self.list.append_front(node)
+                self.map[key] = node
+                pass
+            # still has capacity
+            else:
+                self.list.append_front(node)
+                self.map[key] = node
+
+    def print(self):
+        self.list.print()
+
+
+if __name__ == '__main__':
+    cache = LRUCache(2)
+    cache.put(2,2)
+    cache.print()
+    cache.put(1,1)
+    cache.print()
+    cache.put(3,3)
+    cache.print()
+    print(cache.get(1))
+    cache.print()
+    print(cache.get(2))
+    cache.print()
+    print(cache.get(3))
+    cache.print()
